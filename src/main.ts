dd20c7456699213a58404fd11ea894083c8c1a95
Abduhamid <karalevstvabitva@gmail.com>
2025-09-11 18:23:47 +0500

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,47 @@
-import AppService from './api/app.service';
+// import AppService from './api/app.service';
 
-void AppService.main();
+// void AppService.main();
+
+
+
+
+import { HttpStatus, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './api/app.module';
+import { AllExceptionsFilter } from './infrostructure/exceoption/AllErrorFilter';
+import { NotFoundFilter } from './infrostructure/exceoption/NotFountFilter';
+
+const Port = Number(process.env.PORT) || 4000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new AllExceptionsFilter(), new NotFoundFilter());
+  app.enableCors({ origin: '*' });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Full Stack Loiha')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addSecurityRequirements('bearer', ['bearer'])
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(Port, '0.0.0.0', () => {
+    console.log(`Server running on port ${Port}`);
+  });
+}
+bootstrap();
+
